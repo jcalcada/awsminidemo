6d0067d827391962c002cc455f563ab146b1bd6f
Joe Calcada <jcalcada@pagerduty.com>
2022-08-16 09:25:09 -0400

    back to normal

--- services.js
+++ services.js
@@ -1,13 +1,14 @@
-<!-- var services = {
+var services = {
         "AWS Mini Demo": {
                 "GuardDuty": "02137c00a3b24fb898339ee4d900215f",
                 "CloudWatch": "7af745401e634056a6893d00b234d858"
         }
 }
--->
+<!--
 var services = {
         "AWS Mini Demo": {
                 "GuardDuty": "R02AW55828P7PC1T5B0JMQCHS13V0OC7",
                 "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
         }
 }
+-->
