0ca556e72ea805304ea15a03b65f35831c386673
Joe Calcada <jcalcada@pagerduty.com>
2023-03-09 16:39:10 -0500

    Update services.js

--- services.js
+++ services.js
@@ -1,11 +1,6 @@
 var services = { 
         [
-        "Form Demo":  {
-                "GEO": "R02AW6CU5VG6XCS9PBSU4IKBQ1JALBR5",
-                "CloudWatch": "7af745401e634056a6893d00b234d858"
-        },
-        
-        "AWS Mini Demo": {
+         "AWS Mini Demo": {
                 "GuardDuty": "02137c00a3b24fb898339ee4d900215f",
                 "CloudWatch": "7af745401e634056a6893d00b234d858"
         }
