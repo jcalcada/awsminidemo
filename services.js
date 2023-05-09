08237ddd541b089b0923a93ccfd91de6be4d4ae6
Joe Calcada <jcalcada@pagerduty.com>
2023-05-09 13:31:49 -0400

    Update services.js

--- services.js
+++ services.js
@@ -4,6 +4,7 @@ var services = {
                 "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
          },
          "Major Incident": {
-                "GuardDuty": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCX"
+                "GuardDuty": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCX",
+                "CloudWatch": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCX"
          }
 }
