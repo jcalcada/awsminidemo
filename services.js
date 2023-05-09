e9829ad63261267dcb8332770ffaae422dfe4ab6
Joe Calcada <jcalcada@pagerduty.com>
2023-05-09 09:13:07 -0400

    Update services.js
    changed to GEO

--- services.js
+++ services.js
@@ -1,6 +1,6 @@
 var services = { 
          "AWS Mini Demo": {
-                "GuardDuty": "02137c00a3b24fb898339ee4d900215f",
-                "CloudWatch": "7af745401e634056a6893d00b234d858"
+                "GuardDuty": "R02AW55828P7PC1T5B0JMQCHS13V0OC7",
+                "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
          }
 }
