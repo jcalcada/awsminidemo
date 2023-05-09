9108a891f464d99c09ea5e82264fb310f024084f
Joe Calcada <jcalcada@pagerduty.com>
2023-05-09 12:58:17 -0400

    Update services.js
    adding an option

--- services.js
+++ services.js
@@ -2,5 +2,9 @@ var services = {
          "AWS Mini Demo": {
                 "GuardDuty": "R02AW55828P7PC1T5B0JMQCHS13V0OC7",
                 "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
+         },
+         "Option 2": {
+                "GuardDuty": "R02AW55828P7PC1T5B0JMQCHS13V0OC7",
+                "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
          }
 }
