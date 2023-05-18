8ed13e74e1825c31611952f5bcbfd69b572160e0
Joe Calcada <jcalcada@pagerduty.com>
2023-05-18 11:24:49 -0400

    Update services.js

--- services.js
+++ services.js
@@ -1,10 +1,10 @@
 var services = { 
          "AWS Mini Demo": {
-                "GuardDuty": "R02AW55828P7PC1T5B0JMQCHS13V0OC7",
+                "GuardDuty": "R02DD7L4IC6XNHEV4ACOBA8BI1PQ0A3R",
                 "CloudWatch": "R02AW55828P7PC1T5B0JMQCHS13V0OC7"
          },
          "Major Incident": {
                 "Private": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCXF",
-                "Public": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCX"
+                "Public": "R026AQHIV5CUUVIBMCORPHMQR0K5ADCXF"
          }
 };
